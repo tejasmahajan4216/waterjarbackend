f02622450e4d3c2b36c8e27b7ffbf17ffe9fe765
tejas mahajan <tejasmahajan@gmail.com>
2024-06-24 10:42:40 +0530

    New Update a code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://tejasmahajan4216:OPL7O0YSjznIKFGe@waterjtesting.bcsybhs.mongodb.net/',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
